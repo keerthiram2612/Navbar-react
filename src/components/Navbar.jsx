1326911e1849e7a8e82260fc62dd3f1097de34d5
Keerthana <keerthiram2612@gmail.com>
2023-12-01 19:33:36 +0530

    adding the list with the Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+        <ul>
+            <li>
+              <Link>Home</Link>
+              </li>
+            <li>
+              <Link>About</Link>
+              </li>
+            <li>
+              <Link>Service</Link>
+              </li>
+        </ul>
+    </nav>
   )
 }
