10e50b70cce3f097f0056ec61152da06123969db
Keerthana <keerthiram2612@gmail.com>
2023-12-02 20:11:17 +0530

    creating the condition on by using the useState

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import "./Navbar.css" ;
 export default function Navbar() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <nav>
       <NavLink to = '/' className='title'>Website</NavLink>
-        <ul>
+      <div className='menu' onClick={()=>{
+        setMenuOpen(!menuOpen);
+      }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+        <ul className={menuOpen ? "open" : ""}>
             <li>
               <NavLink to ='/'>Home</NavLink>
               </li>
